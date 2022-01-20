bc1c6f9ec0f18aa8682b5031af309aa0999dc0bc
msolorzanod04 <96075120+msolorzanod04@users.noreply.github.com>
2022-01-20 10:46:12 +0100

    Ejercicio Elementos tabla
    actualizo tras varios errores de compilación con stackblitz

---
+++ README.md
@@ -0,0 +1,3 @@
+# msolorzanod04
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/msolorzanod04)
---
+++ src/data/MenuItems.js
@@ -0,0 +1,39 @@
+import { Home } from '../components/Home';
+import { PaginaListas } from '../components/PaginaListas';
+import { Perfil } from '../components/Perfil';
+import Moviles from '../components/Moviles';
+
+export const MenuItems = [
+  {
+    id: 1,
+    path: '/',
+    title: 'Home',
+    component: Home,
+  },
+  {
+    id: 2,
+    path: '/listas',
+    title: 'Listas de tareas',
+    component: PaginaListas,
+  },
+  {
+    id: 3,
+    path: '/perfil',
+    title: 'Perfil',
+    component: Perfil,
+  },
+  {
+    id: 4,
+    path: '/moviles',
+    title: 'Moviles',
+    component: Moviles,
+  },
+];
+
+/*
+  path
+  id
+  title
+  component
+
+*/
